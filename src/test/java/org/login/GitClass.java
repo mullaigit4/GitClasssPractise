173e3c7e45027463bb1de3dbb31ff473139e790f
mullaigit4 <mullahrtr2@gmail.com>
2023-02-22 17:25:44 +0530

    gokul mtds

--- src/test/java/org/login/GitClass.java
+++ src/test/java/org/login/GitClass.java
@@ -5,49 +5,66 @@ public class GitClass {
 	public void add() {
 		System.err.println("add");
 	}
+
 	public void sub() {
 		System.out.println("sub");
 	}
+
 	public void multiple() {
 		System.out.println("multiple");
 	}
-	
+
 	public void mtd1() {
 		System.out.println("1");
 	}
+
 	public void mtd2() {
 		System.out.println("2");
 	}
+
 	public void mtd3() {
 		System.out.println("3");
 	}
+
 	public void tamilnadu() {
 		System.out.println("tamil");
 	}
+
 	public void kerala() {
 		System.out.println("malayalam");
 	}
+
 	public void andhra() {
 		System.out.println("telugu");
 	}
-	private void tc1() {
+
+	public void tc1() {
 		System.out.println("Testcase1");
 	}
-	private void tc2() {
+
+	public void tc2() {
 		System.out.println("Testcase2");
 	}
-	private void tc3() {
+
+	public void tc3() {
 		System.out.println("Testcase3");
 	}
+
 	public void tc4() {
 		System.out.println("Testcase4");
 	}
+
 	public void tc5() {
 		System.out.println("Testcase5");
 	}
+
 	public void tc6() {
 		System.out.println("Testcase6");
 
-	
-}
+	}
+
+	public void tc7() {
+		System.out.println("Testcase6");
+
+	}
 }
